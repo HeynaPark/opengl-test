783e5e2708a2b45dcc754681ab14c5376d938ef4
hnpark <hnpark@4dreplay.com>
2023-08-17 11:33:53 +0900

    frame + graphic 합성

--- OpenGLGraphicTest/main.cpp
+++ OpenGLGraphicTest/main.cpp
@@ -3,9 +3,13 @@
 
 using namespace cv;
 
-GLuint textureID;
+cv::Mat cvImage = cv::imread("frame.png");
+
+float line_thick = 7.0f;
 cv::Point startPoint(-800, -500);  // 시작점 좌표
 cv::Point endPoint(800, 500);      // 끝점 좌표
+
+void graphicOnFrame();
 
 void saveImage(const char* filename, int width, int height) {
     int channels = 3; // RGB channels
@@ -24,102 +28,101 @@ void saveImage(const char* filename, int width, int height) {
 }
 
 
-void drawLine(Point& start, Point& end) {
-    glLineWidth(4.0f);
+void drawLine(Point2f& start, Point2f& end) {
+    
+    glLineWidth(line_thick);
+    glEnable(GL_LINE_SMOOTH);
     glBegin(GL_LINES);
 
-    glColor3f(1.0f, 0.0f, 0.0f); // 시작점 색상 (빨간색)
-    glVertex2f(start.x / 1920.0f, start.y / 1080.0f);
+    glColor3f(0.0f, 0.0f, 1.0f); // (파란색)
+    glVertex2f(start.x, start.y);
 
-    glColor3f(0.0f, 0.0f, 1.0f); // 끝점 색상 (파란색)
-    glVertex2f(end.x / 1920.0f, end.y / 1080.0f);
-
-
+    glColor3f(1.0f, 0.0f, 0.0f); //  (빨간색)
+    glVertex2f(end.x, end.y);
 
 }
 
-void loadTextureFromMat(const cv::Mat& image) {
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.cols, image.rows, 0, GL_RGB, GL_UNSIGNED_BYTE, image.data);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+float opencvToOpenGLX(int x, int width) {
+    return (2.0f * x / width) - 1.0f;
+}
+
+float opencvToOpenGLY(int y, int height) {
+    return (2.0f * y / height)- 1.0f;
 }
 
 void display() {
+    Point2f start, end;
 
     glClear(GL_COLOR_BUFFER_BIT);
 
+    start.x = opencvToOpenGLX(startPoint.x, 1920);
+    end.x = opencvToOpenGLX(endPoint.x, 1920);
+    
+    start.y = opencvToOpenGLY(startPoint.y, 1080);
+    end.y = opencvToOpenGLY(endPoint.y, 1080);
 
+    std::cout << "point converse -> (start : " << start << " end : " << end << ")" << std::endl;
 
-    drawLine(startPoint, endPoint);
+    drawLine(start, end);
 
     glEnd();
 
-
-    //unsigned char* pixelData = new unsigned char[3 * 1920 * 1080];
-    //glReadPixels(0, 0, 1920, 1080, GL_RGB, GL_UNSIGNED_BYTE, pixelData);
-
-    //// Create an OpenCV Mat from the OpenGL pixel data
-    //cv::Mat openGLImage(1080, 1920, CV_8UC3);
-    //for (int y = 0; y < openGLImage.rows; ++y) {
-    //    for (int x = 0; x < openGLImage.cols; ++x) {
-    //        openGLImage.at<cv::Vec3b>(y, x) = cv::Vec3b(pixelData[3 * (y * openGLImage.cols + x) + 2],
-    //            pixelData[3 * (y * openGLImage.cols + x) + 1],
-    //            pixelData[3 * (y * openGLImage.cols + x)]);
-    //    }
-    //}
-
-    //delete[] pixelData;
-
-    //// Load an OpenCV image
-    //cv::Mat cvImage = cv::imread("frame.png");  // Replace with your image path
-
-    //// Resize the OpenCV image to match the OpenGL rendering resolution
-    //cv::resize(cvImage, cvImage, cv::Size(1920, 1080));
-
-    //// Blend the OpenGL image and the OpenCV image
-    //cv::addWeighted(openGLImage, 1, cvImage, 1, 0.0, openGLImage);
-
-    // Save the final image
- /*   cv::imwrite("final_image.png", openGLImage);
-    cv::imshow("test", openGLImage);
-    waitKey();*/
-
+    graphicOnFrame();
 
     glFlush(); 
-    //saveImage("-.png", 1920, 1080);
-
+    //glutPostRedisplay();
+    //saveImage("golf_line.png", 1920, 1080);
 }
 
-void onMouseClick(int button, int state, int x, int y) {
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        // 마우스 좌표를 OpenGL 좌표로 변환
-        float normalizedX = (2.0f * x) / 1920.0f - 1.0f;
-        float normalizedY = 1.0f - (2.0f * y) / 1080.0f;
-
-        // 첫 번째 점을 마우스 클릭한 위치로 업데이트
-        startPoint = cv::Point(normalizedX * 1920.0f, normalizedY * 1080.0f);
-
-        // 다시 그리기 요청
-        glutPostRedisplay();
+void onMouseClick(int event, int x, int y, int flags, void* userdata) {
+    if (event == cv::EVENT_LBUTTONDOWN) {
+        startPoint = cv::Point(x, y);
+        std::cout << "Left button clicked at (" << x << ", " << y << ")" << std::endl;
     }
-    else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-        // 마우스 좌표를 OpenGL 좌표로 변환
-        float normalizedX = (2.0f * x) / 1920.0f - 1.0f;
-        float normalizedY = 1.0f - (2.0f * y) / 1080.0f;
-
-        // 두 번째 점을 마우스 클릭한 위치로 업데이트
-        endPoint = cv::Point(normalizedX * 1920.0f, normalizedY * 1080.0f);
-
-        // 다시 그리기 요청
-        glutPostRedisplay();
+    else if (event == cv::EVENT_RBUTTONDOWN) {
+        endPoint = cv::Point(x, y);
+        std::cout << "Right button clicked at (" << x << ", " << y << ")" << std::endl;
     }
+}
+
+
+void graphicOnFrame() {
+
+    unsigned char* pixelData = new unsigned char[3 * 1920 * 1080];
+    glReadPixels(0, 0, 1920, 1080, GL_RGB, GL_UNSIGNED_BYTE, pixelData);
+
+    // Create an OpenCV Mat from the OpenGL pixel data
+    cv::Mat openGLImage(1080, 1920, CV_8UC3);
+    for (int y = 0; y < openGLImage.rows; ++y) {
+        for (int x = 0; x < openGLImage.cols; ++x) {
+            openGLImage.at<cv::Vec3b>(y, x) = cv::Vec3b(pixelData[3 * (y * openGLImage.cols + x) + 2],
+                pixelData[3 * (y * openGLImage.cols + x) + 1],
+                pixelData[3 * (y * openGLImage.cols + x)]);
+        }
+    }
+
+    delete[] pixelData;
+
+    // Blend the OpenGL image and the OpenCV image
+    cv::addWeighted(openGLImage, 1, cvImage, 1, 0.0, openGLImage);
+    cv::imshow("frame", openGLImage);
+    waitKey();
+
+    //cv::imwrite("golf_line.png", openGLImage);
 }
 
 
 int main(int argc, char** argv) {
 
+    // get input points
+    cv::resize(cvImage, cvImage, cv::Size(1920, 1080));
+    namedWindow("frame");
+    setMouseCallback("frame", onMouseClick);
+    cv::imshow("frame", cvImage);
+    waitKey(0);
+
+    // draw opengl line
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(1920, 1080);
@@ -128,8 +131,8 @@ int main(int argc, char** argv) {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
     glutDisplayFunc(display);
-    glutMouseFunc(onMouseClick);
-
+    //glutMouseFunc(onMouseClick);
+    
     glutMainLoop();
 
     return 0;
